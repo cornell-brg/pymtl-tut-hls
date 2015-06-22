6fa9f78c58a17c9da96f792335ebffe18ccbd54c
Shreesha Srinath <shreesha@csl.cornell.edu>
2015-06-22 15:37:32 -0400

    [hls-examples] updates to the setup script

--- hls-examples/setup-polyhs-hls.sh
+++ hls-examples/setup-polyhs-hls.sh
@@ -1,2 +1,3 @@
+source /research/brg/install/venv-pkgs/pypy2.5.1-shared/bin/activate
 export PYMTL_VERILATOR_INCLUDE_DIR="${STOW_PKGS_GLOBAL_PREFIX}/share/verilator/include"
 source $BARE_PKGS_GLOBAL_PREFIX/pkgs/xilinx-14.7/Vivado/2014.1/settings64.sh
